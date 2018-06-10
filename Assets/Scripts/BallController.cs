873bbfa8164d70b384662e2763de9d8595576753
INGO-PC\Ingo <i.peters@t-online.de>
2018-06-10 14:41:46 +0200

    Inital Commit - rod with a cylinder platform - ball bouncing always same height

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    bool velocitySaved = false;
+    Vector3 savedVelocity;
+
+    void OnCollisionEnter(Collision collision) {
+        if (!velocitySaved) {
+            savedVelocity = GetComponent<Rigidbody>().velocity;
+            velocitySaved = true;
+        }
+        GetComponent<Rigidbody>().velocity = savedVelocity;
+    }
+}
