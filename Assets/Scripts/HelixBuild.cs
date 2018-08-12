a2a444b06478df879b7362f18d9e24830b3019ba
Ingo Peters <ingo.peters@iav.de>
2018-08-12 19:14:31 +0200

    - another opti of helix build

--- Assets/Scripts/HelixBuild.cs
+++ Assets/Scripts/HelixBuild.cs
@@ -33,7 +33,7 @@ public class HelixBuild : MonoBehaviour {
 	void Start () {
 		
 	}
-	
+
     public void CreatePlatforms(int number) {
         for (int i = 0; i < number; i++) {
             CreatePlatformGameObject();
@@ -59,7 +59,6 @@ public class HelixBuild : MonoBehaviour {
     void DefinePositionOfGap() {
         positionOfGap = Random.Range(0f, 360f);
         //positionOfGap = 0f;
-        print("positionOfGap1 " + positionOfGap);
     }
 
     void CreateNormalBouncingParts() {
@@ -74,18 +73,13 @@ public class HelixBuild : MonoBehaviour {
 
     void CreateTiltParts(int number) {
         GameObject[] myTiltParts = new GameObject[number];
-        //float randomRotationOffset = Random.Range(90f, 180f);
-        float randomRotationOffset = 95f;
-        float rotation = positionOfGap + randomRotationOffset;
         for (int i = 0; i < number; i++) {
             myTiltParts[i] = Instantiate(tiltPartPrefab, platform.transform);
         }
+        float randomRotationOffset = Random.Range(95f, 175f);
+        //float randomRotationOffset = 95f;
+        float rotation = positionOfGap + randomRotationOffset;
         StartCoroutine(RotateOverSeconds(myTiltParts, rotation, buildTime));
-
-        //randomRotationOffset = Random.Range(0f, 90f);
-        randomRotationOffset = 90f;
-        rotation += randomRotationOffset;
-        
     }
 
     void CreatePlatformExitObserver() {
@@ -101,7 +95,7 @@ public class HelixBuild : MonoBehaviour {
         float elaspedTime = 0f;
         float rotatingTime = rotatingTotalTime / rotatingParts.Length;
         int i = rotatingParts.Length;
-        // store the start positions of each rotatinPart
+        // store the start positions of each rotatingPart
         Quaternion[] startPosition = new Quaternion[i];
         for (int j = 0; j < i; j++) {
             startPosition[j] = rotatingParts[j].transform.rotation;
@@ -109,14 +103,17 @@ public class HelixBuild : MonoBehaviour {
         // rotate all parts over time
         while (elaspedTime < rotatingTime) {
             for (int j = 0; j < i; j++) {
-                rotatingParts[j].transform.rotation = Quaternion.Slerp(startPosition[j], Quaternion.AngleAxis(rotation, Vector3.up), (elaspedTime / rotatingTime));
+                float rot = Mathf.Lerp(startPosition[j].y, startPosition[j].y + rotation, (elaspedTime / rotatingTime));
+                rotatingParts[j].transform.rotation = Quaternion.Euler(rotatingParts[j].transform.rotation.x, rot, rotatingParts[j].transform.rotation.z);
+                //rotatingParts[j].transform.rotation = Quaternion.Slerp(startPosition[j], Quaternion.AngleAxis(rotation, Vector3.up), (elaspedTime / rotatingTime));
             }
             elaspedTime += Time.deltaTime;
             yield return new WaitForEndOfFrame();
         }
         // reduce parts by 1 and call the function again for the rest of the parts until there are no parts left
         if (i > 1) {
-            rotation += 90f;
+            rotation = Random.Range(5f, 85f);
+            Debug.Log("rotation " + rotation);
             GameObject[] restOfRotatingParts = new GameObject[i - 1];
             for (int j = 0; j < i - 1; j++) {
                 restOfRotatingParts[j] = rotatingParts[j];
